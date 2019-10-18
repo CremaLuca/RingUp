0615dd8c9688e5086badee315fc8aa2401361765
Luca Crema <>
2019-10-18 17:49:38 +0200

    Creazione demo app per testare l'invio e la ricezione dei messaggi

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -3,40 +3,66 @@ package com.gruppo4.SMSApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
-
-import android.telephony.SmsManager;
 
 import com.gruppo4.sms.SMSController;
 import com.gruppo4.sms.SMSMessage;
 import com.gruppo4.sms.exceptions.InvalidSMSMessageException;
 import com.gruppo4.sms.exceptions.InvalidTelephoneNumberException;
 import com.gruppo4.sms.listeners.SMSRecieveListener;
+import com.gruppo4.sms.listeners.SMSSentListener;
 
-public class MainActivity extends AppCompatActivity implements SMSRecieveListener {
+public class MainActivity extends AppCompatActivity implements SMSRecieveListener, SMSSentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         //Richiediamo il permesso di leggere i messaggi
         requestPermissions(new String[]{Manifest.permission.SEND_SMS},1);
 
         SMSController.addOnReceiveListener(this);
+        SMSController.addOnSentListener(this);
 
-        SMSController controller = new SMSController();
+        Button sendSmileButton = findViewById(R.id.sendSmileButton);
+        sendSmileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSendSmileButton();
+            }
+        });
+    }
+
+    public void onSendSmileButton(){
+        String phoneNumber = ((AutoCompleteTextView)findViewById(R.id.phoneNumberTextView)).getText().toString();
         try {
-            SMSMessage message = new SMSMessage("+393467965447", "Ciao Luca");
-            controller.sendMessage(this, message);
+            SMSMessage message = new SMSMessage(phoneNumber, "Ciao!");
+            SMSController.sendMessage(this, message);
         }catch(InvalidSMSMessageException messageException){
             Log.e("MainActivity",messageException.getMessage());
         }catch(InvalidTelephoneNumberException telephoneException){
+            String error = "";
+            switch (telephoneException.getState()) {
+                case TELEPHONE_NUMBER_TOO_SHORT:
+                    error = "Telephone number is too short!";
+                    break;
+                case TELEPHONE_NUMBER_TOO_LONG:
+                    error="Telephone number is too long!";
+                    break;
+                case TELEPHONE_NUMBER_NO_COUNTRY_CODE:
+                    error = "You have to insert the country code";
+                    break;
+                case TELEPHONE_NUMBER_NOT_A_NUMBER:
+                    error = "The telephone number is not valid";
+                    break;
+            }
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
             Log.e("MainActivity",telephoneException.getMessage());
         }
 
@@ -86,7 +112,19 @@ public class MainActivity extends AppCompatActivity implements SMSRecieveListene
 
     @Override
     public void onSMSRecieve(SMSMessage message) {
-        Log.d("MainActivity","Ricevuto onReceive");
-        Toast.makeText(this,"MAIN ACTIVITY:"+message.getMessageText(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this,message.getTelephoneNumber() + " sent you a smile :)", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSMSSent(SMSController.SMSSentState state) {
+        switch (state){
+            case MESSAGE_SENT:
+                Toast.makeText(this, "Smile sent :)", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.w("MainActivity","Unable to send sms, reason:"+state);
+                Toast.makeText(this, "Unable to send smile :(", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
