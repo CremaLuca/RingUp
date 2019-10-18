2b2f1f89c4e6e76e5d421684db53f63cf681a00f
Alberto <albertoursino98@gmail.com>
2019-10-18 13:18:59 +0200

    Modifiche: - Creazione della classe Message con attributi "numero di telefono" e "testo del messaggio" e i relativi metodi di gestione ad esempio "getMessage", "setMessage"...etc - Crazione dei metodi "writeMessage" e "writeNumber" nella classe SMSController. - Implementazione della classe Message nella classe SMSController.
    Benefici:
    - Classe necessaria: Message
    - Aggiunte personali: metodi "writeMessage" e "writeNumber" per una maggiore facilità di lettura del codice.

--- sms/src/main/java/com/gruppo4/sms/SMSController.java
+++ sms/src/main/java/com/gruppo4/sms/SMSController.java
@@ -4,10 +4,53 @@ import android.telephony.SmsManager;
 
 public class SMSController {
 
-    public void sendMessage(String telephoneNumber, String text){
+    Message message = new Message();
 
-        SmsManager.getDefault().sendTextMessage(telephoneNumber,null,text,null,null);
+    //Scrittura messaggio
+    public void writeMessage(String mex){
+        message.setMessage(mex);
+    }
 
+    //Scrittura numero di telefono
+    public void writeNumber(String num){
+        message.setNumber(num);
+    }
+
+    //Invio del messaggio
+    public void sendMessage(Message message){
+
+        SmsManager.getDefault().sendTextMessage(message.getNumber(),null,message.getMessage(),null,null);
+
+    }
+
+    public void addOnReceiveListener(Message message){
+
+
+    }
+
+}
+
+class Message{
+
+    String text_message;
+    String tel_number;
+
+    Message(){}
+
+    public void setNumber(String number){
+        tel_number = number;
+    }
+
+    public void setMessage(String message){
+        text_message = message;
+    }
+
+    public String getNumber(){
+        return tel_number;
+    }
+
+    public String getMessage(){
+        return text_message;
     }
 
 }
