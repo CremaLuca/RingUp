77e289093c4274eacc3e6d4e75f17d321b0ece5c
Luca Crema <>
2019-10-18 11:02:43 +0200

    Test commit branch

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.gruppo4.SMSApp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.telephony.SmsManager;
 
 import com.gruppo4.sms.SMSController;
 import com.gruppo4.sms.SMSMessage;
@@ -19,8 +26,44 @@ public class MainActivity extends AppCompatActivity implements SMSReceivedListen
     TextView numberTextView, smileReceiver, textView;
     Button smileButton;
 
+    private static final String SENT = "SMS_SENT";
+
+    BroadcastReceiver sentActivity = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            Log.d("MainActivity","WEE ABBIAMO MANDATO UN MESSAGGIO");
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(getBaseContext(), "SMS delivered",
+                            Toast.LENGTH_LONG).show();
+                    break;
+                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                    Toast.makeText(getBaseContext(), "SMS not delivered GENERIC FAILURE",
+                            Toast.LENGTH_LONG).show();
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                    Toast.makeText(getBaseContext(), "SMS not delivered RADIO OFF",
+                            Toast.LENGTH_LONG).show();
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                    Toast.makeText(getBaseContext(), "SMS not delivered NULL PDU",
+                        Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Richiediamo il permesso di leggere i messaggi
+        requestPermissions(new String[]{Manifest.permission.SEND_SMS},1);
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+
+        registerReceiver(sentActivity, new IntentFilter(SENT));
+
+        SMSController controller = new SMSController();
+        controller.sendMessage("+393467965447","Ciao Luca",sentPI);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -65,4 +108,5 @@ public class MainActivity extends AppCompatActivity implements SMSReceivedListen
     public void onMessageReceived(SMSMessage message) {
         smileReceiver.setText(message.getTelephoneNumber() + " " + message.getMessage());
     }
+
 }
--- sms/src/main/java/com/gruppo4/sms/SMSController.java
+++ sms/src/main/java/com/gruppo4/sms/SMSController.java
@@ -1,49 +1,17 @@
 package com.gruppo4.sms;
 
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.telephony.SmsManager;
-
-import com.gruppo4.sms.broadcastReceivers.SMSSentBroadcastReceiver;
-import com.gruppo4.sms.interfaces.SMSReceivedListener;
-import com.gruppo4.sms.interfaces.SMSSentListener;
-
-import java.util.ArrayList;
 
 public class SMSController {
 
-    private static final String SENT_STRING = "SMS_SENT";
-    private static ArrayList<SMSReceivedListener> receivedListeners = new ArrayList<>();
-
-    public SMSController() {
+    public static void sendMessage(String telephoneNumber, String text, PendingIntent onSent){
+        SmsManager.getDefault().sendTextMessage(telephoneNumber,null,text,onSent,null);
     }
 
-    //Send Message
-    public static void sendMessage(SMSMessage message, Context context) {
-        sendMessage(message, context, null);
+    public static void addOnReceiveListener(Context context){
+
     }
 
-    public static void sendMessage(SMSMessage message, Context context, SMSSentListener listener) {
-        SmsManager sms = SmsManager.getDefault();
-        //Register the receiver for this message
-        String sentString = SENT_STRING + "_" + message.getTelephoneNumber();
-        BroadcastReceiver receiver = new SMSSentBroadcastReceiver(message, listener);
-        context.registerReceiver(receiver, new IntentFilter(sentString));
-
-        PendingIntent sentPI = PendingIntent.getBroadcast(context, 0, new Intent(sentString), 0);
-        sms.sendTextMessage(message.getTelephoneNumber(), null, message.getMessage(), sentPI, null);
-    }
-
-    public static void addOnReceivedListener(SMSReceivedListener smsReceivedListener) {
-        receivedListeners.add(smsReceivedListener);
-    }
-
-    public static void callReceivedListener(SMSMessage message) {
-        for (SMSReceivedListener listener : receivedListeners) {
-            listener.onMessageReceived(message);
-        }
-    }
 }
