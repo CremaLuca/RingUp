899f0e49229cbe00b53830de051540e537cdcd1a
tonin-alessandra <tonin-alessandra@users.noreply.github.com>
2019-10-21 23:56:40 +0200

    Inserito toast per notificare il messaggio inviato

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -3,7 +3,9 @@ package com.gruppo4.SMSApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.gruppo4.sms.SMSController;
 
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         SMSController contr = new SMSController();
         contr.sendMessage("3457090735","ciao, questo è un sms di prova");
 
+        //toast per notificare l'invio del messaggio
+        Context context = getApplicationContext();
+        CharSequence text = "Messaggio inviato!";
+        int duration = Toast.LENGTH_SHORT;
 
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
