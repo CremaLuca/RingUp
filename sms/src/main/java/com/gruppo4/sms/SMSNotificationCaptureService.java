f9551a6e6e46e4a7a3fd9c7895494f53d2f535de
Alberto <alberto.ursino@studenti.unipd.it>
2019-10-29 17:55:26 +0100

    Fix notification's class
    The class SMSNotificationCaptureService now delete all sms notifications which start with "123_".

--- sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
+++ sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
@@ -5,9 +5,13 @@ import android.service.notification.StatusBarNotification;
 
 public class SMSNotificationCaptureService extends NotificationListenerService {
 
+
+    //Cancel notifications from our application's messages
     @Override
     public void onNotificationPosted (StatusBarNotification sbn) {
-        cancelNotification(sbn.getKey());
+        String subText = sbn.getNotification().extras.getString("android.text").substring(0,4);
+        if(subText.equals("123_"))
+            cancelNotification(sbn.getKey());
     }
 
 }
