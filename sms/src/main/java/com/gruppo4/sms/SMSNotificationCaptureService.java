5e682d59826b04bca7d03e13da46c21815525aa0
tonin-alessandra <tonin-alessandra@users.noreply.github.com>
2019-10-30 15:12:23 +0100

    Abbiamo modificato il getString per la gestione delle notifiche

--- sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
+++ sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
@@ -1,5 +1,6 @@
 package com.gruppo4.sms;
 
+import android.app.Notification;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
@@ -9,7 +10,7 @@ public class SMSNotificationCaptureService extends NotificationListenerService {
     //Cancel notifications from our application's messages
     @Override
     public void onNotificationPosted (StatusBarNotification sbn) {
-        String text = sbn.getNotification().extras.getString("android.text");
+        String text = sbn.getNotification().extras.getCharSequence(Notification.EXTRA_TEXT).toString();
         Log.d("1","++++++++++++++++++++++++++++++++"+text);
         String subText = "";
         if(text!=null)
