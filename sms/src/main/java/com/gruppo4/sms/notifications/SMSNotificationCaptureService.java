a116509a8ac18f6ddf29be2f0c3e0075f1f9be6c
Luca Crema <>
2019-10-30 18:43:57 +0100

    Code review e documentazione

--- sms/src/main/java/com/gruppo4/sms/notifications/SMSNotificationCaptureService.java
+++ sms/src/main/java/com/gruppo4/sms/notifications/SMSNotificationCaptureService.java
@@ -1,16 +1,22 @@
-package com.gruppo4.sms;
+package com.gruppo4.sms.notifications;
 
 import android.app.Notification;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import com.gruppo4.sms.SMSPacket;
+
 public class SMSNotificationCaptureService extends NotificationListenerService {
 
+    /**
+     * Called every time a notification pops up, cancels the notification if it is for the sms library
+     *
+     * @param sbn Notification coming from the status bar
+     */
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         String notificationText;
-        String subText = "";
         Notification notification = sbn.getNotification();
 
         if (notification != null) {
@@ -18,13 +24,20 @@ public class SMSNotificationCaptureService extends NotificationListenerService {
                 if (notification.category.equals(Notification.CATEGORY_MESSAGE)) { //if the notification is a message
                     Log.d("SMSNotifCaptureSVC", "Notification category is " + notification.category);
                     notificationText = notification.extras.getCharSequence(Notification.EXTRA_TEXT).toString();
-                    String[] parts = notificationText.split(SMSPacket.SEPARATOR, 5);
-                    if (parts.length >= 5) {
+                    if (checkNotificationText(notificationText)) {
                         Log.d("SMSNotifCaptureSVC", "Notification canceled");
                         cancelNotification(sbn.getKey());
                     }
                 }
             }
         }
+    }
+
+    /**
+     * @return if the notification of category msg is suitable to be canceled
+     */
+    private boolean checkNotificationText(String notificationText) {
+        String[] parts = notificationText.split(SMSPacket.SEPARATOR, 5);
+        return parts.length >= 5;
     }
 }
