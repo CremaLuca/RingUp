ec561df7821a51aefd17bb72b27a093fe26209c8
Alberto <alberto.ursino@studenti.unipd.it>
2019-10-30 15:05:21 +0100

    Nothing change

--- sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
+++ sms/src/main/java/com/gruppo4/sms/SMSNotificationCaptureService.java
@@ -2,16 +2,22 @@ package com.gruppo4.sms;
 
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 public class SMSNotificationCaptureService extends NotificationListenerService {
-
 
     //Cancel notifications from our application's messages
     @Override
     public void onNotificationPosted (StatusBarNotification sbn) {
-        String subText = sbn.getNotification().extras.getString("android.text").substring(0,4);
-        if(subText.equals("123_"))
+        String text = sbn.getNotification().extras.getString("android.text");
+        Log.d("1","++++++++++++++++++++++++++++++++"+text);
+        String subText = "";
+        if(text!=null)
+            subText = text.substring(0,4);
+        Log.d("1","++++++++++++++++++++++++++++++++"+subText);
+        if(subText.equals("123_")) {
             cancelNotification(sbn.getKey());
+        }
     }
 
 }
