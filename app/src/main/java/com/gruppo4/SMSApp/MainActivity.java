29bbcff6bf7531921c754c015e92a2c7ec040fe0
Alessandra Tonin <ale.tonin.98@gmail.com>
2019-11-06 11:57:56 +0100

    Added class RingtoneHandler
    the phone rings, nut it doesn't stops

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.gruppo4.SMSApp;
 
+import android.content.Context;
+import android.media.Ringtone;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.gruppo4.ringtonePlayer.RingtoneHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    * when button clicked, play the ringtone
+    * @param the layout button
+     */
+    public void onRingClick(View view){
+
+        Context context = getApplicationContext();
+        Ringtone ringtone = RingtoneHandler.getDefaultRingtone(context);
+        RingtoneHandler.ringtonePlay(ringtone);
+    }
+
+    /*
+    public void onStopClick(View view){
+
+        Context context = getApplicationContext();
+        Ringtone ringtone = RingtoneHandler.getDefaultRingtone(context);
+        RingtoneHandler.ringtoneStop(ringtone);
+    }
+
+     */
 
 }
---
+++ app/src/main/java/com/gruppo4/ringtonePlayer/RingtoneHandler.java
@@ -0,0 +1,42 @@
+package com.gruppo4.ringtonePlayer;
+
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+public class RingtoneHandler {
+
+    /**
+    * get the user's default ringtone
+    * @param ctx, the current application context
+    * @return the default ringtone
+     */
+    public static Ringtone getDefaultRingtone(Context ctx){
+        Uri alarmTone = RingtoneManager.getActualDefaultRingtoneUri(ctx, RingtoneManager.TYPE_RINGTONE);
+        Ringtone ringtone = RingtoneManager.getRingtone(ctx, alarmTone);
+        return ringtone;
+    }
+
+    /**
+    * plays the default ringtone
+    * @param ringtone, the default ringtone
+     */
+    public static void ringtonePlay(Ringtone ringtone){
+        ringtone.setAudioAttributes(new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ALARM).build());
+        if(ringtone!=null)
+            ringtone.play();
+    }
+
+    /*
+    public static void ringtoneStop(Ringtone ringtone){
+        ringtone.stop();
+    }
+    
+     */
+
+
+
+
+}
