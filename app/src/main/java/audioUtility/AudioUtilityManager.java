95b07629af86cb696c91e555947d5e7f82918b95
francescobau <56580849+francescobau@users.noreply.github.com>
2019-11-06 14:08:41 +0100

    Second commit: test app works. Moved everything into AudioUtilityManager, cleaned MainActivity and successfully implemented the automatic setup of the volume in 1 row.
    AudioUtilityManager:
    - method getAudioManager(Context) to get the AudioManager from a certain Context
    - method setVolumeManager(Context) to set the volume to 50%.
    MainActivity:
    In onCreate, only 1 row to set the ringtone volume to 50% automatically.
    
    What's missing: method getCurrentVolume() to check the current volume. It was implemented before on a click of the button.

--- app/src/main/java/audioUtility/AudioUtilityManager.java
+++ app/src/main/java/audioUtility/AudioUtilityManager.java
@@ -1,33 +1,24 @@
 package audioUtility;
-import android.Manifest;
+
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gruppo4.SMSApp.R;
+
 
 public class AudioUtilityManager extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Get the application context
-        mContext = getApplicationContext();
-        mActivity = this;
-
-        // Get the audio manager instance
-        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-
-        //Setup the volume.
-        setupVolumeManager();
-    }
 
     //private static final int AUDIO_SETTINGS_PERMISSION_CODE = 1;
 
@@ -39,7 +30,61 @@ public class AudioUtilityManager extends AppCompatActivity {
     private RecyclerView listView;
 
 
-    private AudioManager mAudioManager;
+    private NotificationManager mNotificationManager;
+
+    //Meglio non mettere static, altrimenti crasha
+    //private AudioManager mAudioManager;
+
+
+    private TextView mTextView;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the application context
+        mContext = getApplicationContext();
+        mActivity = this;
+
+        // Connect RecyclerView, Button and TextView
+        listView = findViewById(R.id.recyclerView);
+        mTextView = findViewById(R.id.hello_world);
+        mButton = findViewById(R.id.current_volume_button);
+
+        // Listener for the button
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // HARDWIRING...
+                onButtonClick(getApplicationContext(), 1);
+            }
+        });
+
+        // Get the notification manager instance
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+
+        //Setup the volume.
+        setupVolumeManager(this);
+    }
+
+    private void onButtonClick(Context applicationContext, int internalCode) {
+
+        // Get the audio manager instance
+        AudioManager mAudioManager = getAudioManager(applicationContext);
+
+        // Get the ringer current volume level
+        int current_volume_level = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+        // Show it on the Toast
+        Toast.makeText(applicationContext, "CURRENT RINGTONE VOLUME: "+current_volume_level, Toast.LENGTH_SHORT).show();
+
+        // Show it on the TextView (experimental, and I don't know how to do it... Should I use the Adapter?
+
+    }
+
 
     /*
     private boolean checkVolumePermission() {
@@ -47,29 +92,28 @@ public class AudioUtilityManager extends AppCompatActivity {
 
     }
     */
+    public static AudioManager getAudioManager(Context context){
+        // Get the audio manager instance
+        return (AudioManager) context.getSystemService(AUDIO_SERVICE);
+    }
 
-    private void setupVolumeManager() {
+    public static void setupVolumeManager(Context context) {
         // If do not disturb mode on, then off it first
         // turnOffDoNotDisturbMode();
         // NON FUNZIONA. SERVE METODO ALTERNATIVO PER BYPASSARE IL DND.
 
-        /*
-        // Get the ringer current volume level
-        int current_volume_level = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
-         */
-
         // Get the ringer maximum volume
-        int max_volume_level = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+        int max_volume_level = getAudioManager(context).getStreamMaxVolume(AudioManager.STREAM_RING);
 
         // calculate approx. 50% of the volume
         int half_volume = max_volume_level/2;
         // Set the ringer volume
-        mAudioManager.setStreamVolume(
+        getAudioManager(context).setStreamVolume(
                 AudioManager.STREAM_RING,
                 half_volume,
                 AudioManager.FLAG_SHOW_UI
         );
-        Toast.makeText(this, "RINGTONE AUDIO IS SET TO 50%.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "RINGTONE AUDIO IS SET TO 50%.", Toast.LENGTH_SHORT).show();
 //        Context ctx = getApplicationContext();
         ;
         /*
--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -1,93 +1,23 @@
 package com.gruppo4.SMSApp;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.media.AudioManager;
 import android.os.Bundle;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ListAdapter;
-import androidx.recyclerview.widget.RecyclerView;
-import com.gruppo4.SMSApp.R;
 
+import audioUtility.AudioUtilityManager;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    //private static final int AUDIO_SETTINGS_PERMISSION_CODE = 1;
-
-    private Context mContext;
-    private Activity mActivity;
-
-    // Attualmente non so come usarlo. Intanto mi occupo dei permessi, poi ci si occuperà
-    // della View.
-    private RecyclerView listView;
-
-
-    private NotificationManager mNotificationManager;
-    private AudioManager mAudioManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.gruppo4.SMSApp.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        // Get the application context
-        mContext = getApplicationContext();
-        mActivity = MainActivity.this;
 
-        // Get the notification manager instance
-        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-        // Get the audio manager instance
-        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-
-        //Setup the volume.
-        setupVolumeManager();
+        // Sets up the volume at 50%
+        AudioUtilityManager.setupVolumeManager(this);
     }
 
-
-    /*
-    private boolean checkVolumePermission() {
-        return checkSelfPermission(this,AudioManager.);
-
-    }
-    */
-
-    private void setupVolumeManager() {
-        // If do not disturb mode on, then off it first
-        // turnOffDoNotDisturbMode();
-        // NON FUNZIONA. SERVE METODO ALTERNATIVO PER BYPASSARE IL DND.
-
-        /*
-        // Get the ringer current volume level
-        int current_volume_level = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
-         */
-
-        // Get the ringer maximum volume
-        int max_volume_level = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
-
-        // calculate approx. 50% of the volume
-        int half_volume = max_volume_level/2;
-        // Set the ringer volume
-        mAudioManager.setStreamVolume(
-                AudioManager.STREAM_RING,
-                half_volume,
-                AudioManager.FLAG_SHOW_UI
-        );
-        Toast.makeText(this, "RINGTONE AUDIO IS SET TO 50%.", Toast.LENGTH_SHORT).show();
-//        Context ctx = getApplicationContext();
-        ;
-        /*
-        if (!AudioManager.getInstance(ctx).isSetup()) {
-            AudioManager.getInstance(ctx).setup(APP_ID);
-        }
-        AudioManager.getInstance(ctx).addReceivedMessageListener(this);
-        */
-    }
 
 }
