4ede3c731ff2bfb3170edce41398b029fd226344
Luca Crema <lc.crema@hotmail.com>
2019-11-07 23:33:00 +0100

    Skeleton of the application. Missing the parts from branches features

---
+++ app/src/main/java/com/gruppo4/SMSApp/AppManager.java
@@ -0,0 +1,25 @@
+package com.gruppo4.SMSApp;
+
+public class AppManager {
+
+    /*
+    public static void onRingCommandReceived(Context ctx, RingCommand command){
+        if(RingHandler.checkPassword(ctx, command){
+            RingtoneHandler.playRingtone(15);
+        }
+    }
+     */
+
+    /*
+    public static void sendCommand(Context ctx, RingCommand command, SMSSentListener listener){
+        SMSMessage message = commandToMessage(command);
+        SMSManager.getInstance(ctx).sendMessage(message,listener);
+
+    }
+
+    private static SMSMessage commandToMessage(RingCommand command){
+        return new SMSMessage(command.getPeer(),RingtoneHandler.parseCommand(command));
+    }
+     */
+
+}
--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -4,12 +4,28 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gruppo4.sms.dataLink.SMSManager;
+
+/**
+ * @author Gruppo 4
+ */
 public class MainActivity extends AppCompatActivity {
+
+    public void setup() {
+        //Initialize the receiver
+        SMSManager.getInstance(this).addReceivedMessageListener(new ReceivedMessageListener(this));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void sendRingCommand(String destination, String password) {
+        //RingCommand command = new RingCommand(new SMSPeer(destination), password);
+        //AppManager.sendCommand(this,command,new SMSSentListener(){})
+        //TODO : Dentro al listener un toast per dire che il comando è stato inviato
     }
 
 }
