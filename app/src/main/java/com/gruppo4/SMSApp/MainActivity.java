d67415a2394ca1accb9587e1e70142c17798fad0
Alessandra Tonin <ale.tonin.98@gmail.com>
2019-11-08 15:15:45 +0100

    Fix specifications

--- app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
+++ app/src/main/java/com/gruppo4/SMSApp/MainActivity.java
@@ -9,17 +9,19 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gruppo4.ringtonePlayer.RingtoneHandler;
-/*
-* @author Alessandra Tonin
+
+/**
+ * @author Alessandra Tonin
  */
+
 public class MainActivity extends AppCompatActivity {
 
 
     Ringtone ringtone = null;
     Ringtone alarmtone = null;
 
-    /*
-    * when app opens, get the current alarm tone and ringtone
+    /**
+     * when app opens, get the current alarm tone and ringtone
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,42 +33,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
-    * when button "ringtone" clicked, plays the ringtone if the alarm isn't ringing
-    * @param the layout button
+    /**
+     * when button "ringtone" clicked, plays the ringtone if the alarm isn't ringing
+     *
+     * @param view, the layout button
      */
-    public void onRingClick(View view){
+    public void onRingClick(View view) {
 
-        if(!(alarmtone.isPlaying()))
+        if (!(alarmtone.isPlaying()))
             RingtoneHandler.ringtonePlay(ringtone);
         else
             Toast.makeText(this, "The alarm tone is already playing, please stop it and retry!", Toast.LENGTH_SHORT).show();
 
     }
 
-    /*
+    /**
      * when button "alarm" clicked, plays the alarm tone if the ringtone isn't ringing
-     * @param the layout button
+     *
+     * @param view, the layout button
      */
-    public void onAlarmClick(View view){
-        if(!(ringtone.isPlaying()))
+    public void onAlarmClick(View view) {
+        if (!(ringtone.isPlaying()))
             RingtoneHandler.ringtonePlay(alarmtone);
         else
             Toast.makeText(this, "The ringtone is already playing, please stop it and retry!", Toast.LENGTH_SHORT).show();
     }
 
-    /*
+    /**
      * when button "stop ringing!" clicked, stops the sound that's playing
-     * @param the layout button
+     *
+     * @param view, the layout button
      */
-    public void onStopClick(View view){
-        if(ringtone.isPlaying())
+    public void onStopClick(View view) {
+        if (ringtone.isPlaying())
             RingtoneHandler.ringtoneStop(ringtone);
 
-        if(alarmtone.isPlaying())
+        if (alarmtone.isPlaying())
             RingtoneHandler.ringtoneStop(alarmtone);
     }
-
 
 
 }
--- app/src/main/java/com/gruppo4/ringtonePlayer/RingtoneHandler.java
+++ app/src/main/java/com/gruppo4/ringtonePlayer/RingtoneHandler.java
@@ -6,18 +6,19 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 
-/*
+/**
  * @author Alessandra Tonin
  */
 
 public class RingtoneHandler {
 
     /**
-    * get the user's ringtone
-    * @param ctx, the current application context
-    * @return the ringtone
+     * get the user's ringtone
+     *
+     * @param ctx, the current application context
+     * @return the ringtone
      */
-    public static Ringtone getDefaultRingtone(Context ctx){
+    public static Ringtone getDefaultRingtone(Context ctx) {
         Uri ringtoneId = RingtoneManager.getActualDefaultRingtoneUri(ctx, RingtoneManager.TYPE_RINGTONE);
         Ringtone ringtone = RingtoneManager.getRingtone(ctx, ringtoneId);
         return ringtone;
@@ -25,36 +26,35 @@ public class RingtoneHandler {
 
     /**
      * get the user's alarm tone
+     *
      * @param ctx, the current application context
      * @return the alarm tone
      */
-    public static Ringtone getDefaultAlarmTone(Context ctx){
+    public static Ringtone getDefaultAlarmTone(Context ctx) {
         Uri alarmToneId = RingtoneManager.getActualDefaultRingtoneUri(ctx, RingtoneManager.TYPE_ALARM);
         Ringtone alarmTone = RingtoneManager.getRingtone(ctx, alarmToneId);
         return alarmTone;
     }
 
     /**
-    * plays a ringtone
-    * @param ringtone, the default ringtone
+     * plays a ringtone
+     *
+     * @param ringtone, the default ringtone
      */
-    public static void ringtonePlay(Ringtone ringtone){
+    public static void ringtonePlay(Ringtone ringtone) {
         ringtone.setAudioAttributes(new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ALARM).build());
-        if(ringtone!=null)
+        if (ringtone != null)
             ringtone.play();
     }
 
     /**
      * stops the playing sound
+     *
      * @param ringtone, the playing sound (ringtone or alarm tone)
      */
-    public static void ringtoneStop(Ringtone ringtone){
+    public static void ringtoneStop(Ringtone ringtone) {
         ringtone.stop();
     }
-    
-
-
-
 
 
 }
