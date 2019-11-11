2488720f0a2417ff36afd8e0b57ec3f78b1d731f
francescobau <francescobau@users.noreply.github.com>
2019-11-11 18:39:18 +0100

    [FAIL] Attempted to do Unit-Test, Reformatted code (CTRL+ALT+L)

--- app/src/main/java/com/gruppo4/audioUtility/AudioUtilityManager.java
+++ app/src/main/java/com/gruppo4/audioUtility/AudioUtilityManager.java
@@ -16,56 +16,52 @@ public class AudioUtilityManager {
     public static final int MIN_PERCENTAGE = 0;
 
     /**
-     *
      * @param context
      * @return The current Alarm Volume (in percentage).
      */
-    public static int getCurrentAlarmVolume(Context context){
+    public static int getCurrentAlarmVolume(Context context) {
 
         int currentVolume = getAudioManager(context).getStreamVolume(AudioManager.STREAM_ALARM);
 
         int maxVolume = getMaxAlarmVolume(context);
-        return Math.round(MAX_PERCENTAGE*currentVolume/maxVolume);
+        return Math.round(MAX_PERCENTAGE * currentVolume / maxVolume);
     }
 
     /**
-     *
      * @param context
      * @return The maximum Alarm volume (real value).
      */
-    private static int getMaxAlarmVolume(Context context){
+    private static int getMaxAlarmVolume(Context context) {
         return getAudioManager(context).getStreamMaxVolume(AudioManager.STREAM_ALARM);
     }
 
     /**
-     *
      * @param context
      * @return The current AudioManager instance, determined by a certain Context.
      */
-    public static AudioManager getAudioManager(Context context){
+    public static AudioManager getAudioManager(Context context) {
         return (AudioManager) context.getSystemService(AUDIO_SERVICE);
     }
 
     /**
-     *
      * @param context
      * @param percentage
      * @throws IllegalArgumentException if percentage is not between 0 and 100.
-     * Sets up the Alarm Volume, given a certain percentage.
+     *                                  Sets up the Alarm Volume, given a certain percentage.
      */
-    public static void setAlarmVolume(Context context, int percentage) throws IllegalArgumentException{
+    public static void setAlarmVolume(Context context, int percentage) throws IllegalArgumentException {
 
-        if(percentage<MIN_PERCENTAGE)
+        if (percentage < MIN_PERCENTAGE)
             throw new IllegalArgumentException(" Your value is too low. Please insert a value between 0 and 100.");
-        else if(percentage>MAX_PERCENTAGE) {
+        else if (percentage > MAX_PERCENTAGE) {
             throw new IllegalArgumentException(" Your value is too high. Please insert a value between 0 and 100.");
         }
 
         int maxVolume = getMaxAlarmVolume(context);
 
         // Calculate the real value of the new volume
-        int newVolume = maxVolume*percentage;
-        newVolume = Math.round(newVolume/MAX_PERCENTAGE);
+        int newVolume = maxVolume * percentage;
+        newVolume = Math.round(newVolume / MAX_PERCENTAGE);
 
         // Set the Alarm volume
         getAudioManager(context).setStreamVolume(
@@ -77,18 +73,16 @@ public class AudioUtilityManager {
     }
 
     /**
-     * @param context
-     * Sets up the Alarm Volume to its maximum value (in percentage).
+     * @param context Sets up the Alarm Volume to its maximum value (in percentage).
      */
-    public static void setMaxAlarmVolume(Context context){
+    public static void setMaxAlarmVolume(Context context) {
         setAlarmVolume(context, MAX_PERCENTAGE);
     }
 
     /**
-     * @param context
-     * Sets up the Alarm Volume to its minimum value (in percentage).
+     * @param context Sets up the Alarm Volume to its minimum value (in percentage).
      */
-    public static void setMinAlarmVolume(Context context){
+    public static void setMinAlarmVolume(Context context) {
         setAlarmVolume(context, MIN_PERCENTAGE);
     }
 
--- app/src/test/java/com/gruppo4/SMSApp/AudioUtilityManagerUnitTest.java
+++ app/src/test/java/com/gruppo4/SMSApp/AudioUtilityManagerUnitTest.java
@@ -3,6 +3,7 @@ package com.gruppo4.SMSApp;
 /**
  * @author Francesco Bau'
  */
+
 import android.content.Context;
 
 import com.gruppo4.audioUtility.AudioUtilityManager;
@@ -11,26 +12,28 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AudioUtilityManagerUnitTest extends junit.framework.TestCase{
+public class AudioUtilityManagerUnitTest extends junit.framework.TestCase {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void setAlarmVolume_percentage_isTooLow(Context context) {
+        try {
+            AudioUtilityManager.setAlarmVolume(context, -1);
+            Assert.fail("Should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //Success
+        } catch (Exception x) {
+            Assert.fail(x.getStackTrace() + "\nShould have thrown IllegalArgumentException");
+        }
     }
 
     @Test
-    public void percentage_isTooLow(Context context){
-        try{
-            AudioUtilityManager.setAlarmVolume(context,-1);
-            Assert.fail("Should have thrown IllegalArgumentException");
-        }catch(IllegalArgumentException e){
-            //Success
+    public void setAlarmVolume_percentage_isTooHigh(Context context) {
+        try {
+            AudioUtilityManager.setAlarmVolume(context, 101);
+        } catch (IllegalArgumentException e) {
+            // Success
+        } catch (Exception x) {
+            Assert.fail(x.getStackTrace() + "\nShould have thrown IllegalArgumentException");
         }
-        catch (Exception x){
-            Assert.fail(x.getStackTrace()+"\nShould have thrown IllegalArgumentException");
-        }
-    }
-    @Test
-    public void percentage_isTooHigh(Context context){
-        AudioUtilityManager.setAlarmVolume(context,101);
     }
 }
