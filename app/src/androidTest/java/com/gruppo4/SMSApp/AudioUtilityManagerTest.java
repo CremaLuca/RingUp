841272089099ca34925e36471637e8f6787c0a92
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2019-11-11 18:55:37 +0100

    Fixed tests written by Francesco Bau'
    - He was trying to do Unit Testing with a context.
    - Removed the extension of the class "junit.framework.TestCase" from the tests class.

--- app/src/androidTest/java/com/gruppo4/SMSApp/AudioUtilityManagerTest.java
+++ app/src/androidTest/java/com/gruppo4/SMSApp/AudioUtilityManagerTest.java
@@ -1,21 +1,27 @@
 package com.gruppo4.SMSApp;
 
 /**
- * @author Francesco Bau'
+ * @author Francesco Bau', Alberto Ursino
  */
 
 import android.content.Context;
-
+import androidx.test.platform.app.InstrumentationRegistry;
 import com.gruppo4.audioUtility.AudioUtilityManager;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AudioUtilityManagerUnitTest extends junit.framework.TestCase {
+public class AudioUtilityManagerTest {
+
+    Context context;
+
+    @Before
+    public void init() {
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
 
     @Test
-    public void setAlarmVolume_percentage_isTooLow(Context context) {
+    public void setAlarmVolume_percentage_isTooLow() {
         try {
             AudioUtilityManager.setAlarmVolume(context, -1);
             Assert.fail("Should have thrown IllegalArgumentException");
@@ -27,7 +33,7 @@ public class AudioUtilityManagerUnitTest extends junit.framework.TestCase {
     }
 
     @Test
-    public void setAlarmVolume_percentage_isTooHigh(Context context) {
+    public void setAlarmVolume_percentage_isTooHigh() {
         try {
             AudioUtilityManager.setAlarmVolume(context, 101);
         } catch (IllegalArgumentException e) {
