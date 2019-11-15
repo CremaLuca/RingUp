31684dec3fa77320b313ac472b02f28b923f69e1
francescobau <56580849+francescobau@users.noreply.github.com>
2019-11-15 20:36:13 +0100

    Deleted methods setLoudMode(Context) and setQuietMode(Context), added enum Stream, added method getStream(Context,Stream), optimized methods thanks to enum Stream, updated specifications (reduces throws declarations)

--- app/src/main/java/com/gruppo4/audioUtility/AudioUtilityManager.java
+++ app/src/main/java/com/gruppo4/audioUtility/AudioUtilityManager.java
@@ -15,9 +15,11 @@ public class AudioUtilityManager {
     public static final int MAX_PERCENTAGE = 100;
     public static final int MIN_PERCENTAGE = 0;
 
-    public static final String ALARM = "ALARM";
-    public static final String RING = "RING";
-    public static final String MUSIC = "MUSIC";
+    public enum Stream {
+        ALARM,
+        RING,
+        MUSIC
+    }
 
     /**
      * @param context The current Context
@@ -29,29 +31,11 @@ public class AudioUtilityManager {
 
     /**
      * @param context The current Context
-     * @param stream  The chosen Stream (should be"ALARM"/"RING"/"MUSIC", otherwise an exception is thrown)
+     * @param stream  The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
      * @return The current Stream Volume (in percentage).
-     * @throws IllegalArgumentException if parameter Stream is illegal
      */
-    public static int getVolume(Context context, String stream) throws IllegalArgumentException {
-        int currentVolume;
-        switch (stream) {
-            case ALARM: {
-                currentVolume = getAudioManager(context).getStreamVolume(AudioManager.STREAM_ALARM);
-                break;
-            }
-            case RING: {
-                currentVolume = getAudioManager(context).getStreamVolume(AudioManager.STREAM_RING);
-                break;
-            }
-            case MUSIC: {
-                currentVolume = getAudioManager(context).getStreamVolume(AudioManager.STREAM_MUSIC);
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("\nIllegal choice of Stream. Choose between \"ALARM\"," +
-                        " \"RING\" or \"MUSIC\" next time.");
-        }
+    public static int getVolume(Context context, Stream stream) {
+        int currentVolume = getAudioManager(context).getStreamVolume(getStream(context, stream));
 
         int maxVolume = getMaxVolume(context, stream);
         return Math.round(MAX_PERCENTAGE * currentVolume / maxVolume);
@@ -59,38 +43,22 @@ public class AudioUtilityManager {
 
     /**
      * @param context The current Context
-     * @param stream  The chosen Stream (should be"ALARM"/"RING"/"MUSIC", otherwise an exception is thrown)
+     * @param stream  The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
      * @return The maximum Stream volume (real value).
-     * @throws IllegalArgumentException if parameter Stream is illegal
      */
-    private static int getMaxVolume(Context context, String stream) throws IllegalArgumentException {
-        String streamUP = stream.toUpperCase();
-        switch (streamUP) {
-            case ALARM: {
-                return getAudioManager(context).getStreamMaxVolume(AudioManager.STREAM_ALARM);
-            }
-            case RING: {
-                return getAudioManager(context).getStreamMaxVolume(AudioManager.STREAM_RING);
-            }
-            case MUSIC: {
-                return getAudioManager(context).getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-            }
-            default:
-                throw new IllegalArgumentException("\nIllegal choice of Stream. Choose between \"ALARM\"," +
-                        " \"RING\" or \"MUSIC\" next time.");
-        }
+    private static int getMaxVolume(Context context, Stream stream) {
+        return getAudioManager(context).getStreamMaxVolume(getStream(context, stream));
     }
 
     /**
      * Sets up the Stream Volume, given a certain percentage.
      *
      * @param context    The current Context
-     * @param stream     The chosen Stream (should be"ALARM"/"RING"/"MUSIC", otherwise an exception is thrown)
+     * @param stream     The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
      * @param percentage Target volume (expressed in %).
      * @throws IllegalArgumentException if percentage is not between 0 and 100.
-     * @throws IllegalArgumentException if parameter Stream is illegal.
      */
-    public static void setVolume(Context context, String stream, int percentage) throws IllegalArgumentException {
+    public static void setVolume(Context context, Stream stream, int percentage) throws IllegalArgumentException {
         if (percentage < MIN_PERCENTAGE)
             throw new IllegalArgumentException(" Your value is too low. Please insert a value between 0 and 100.");
         else if (percentage > MAX_PERCENTAGE) {
@@ -101,38 +69,13 @@ public class AudioUtilityManager {
         // Calculate the real value of the new volume
         int newVolume = maxVolume * percentage;
         newVolume = Math.round(newVolume / MAX_PERCENTAGE);
-        switch (stream) {
-            case ALARM: {
-                // Set the Alarm volume
-                getAudioManager(context).setStreamVolume(
-                        AudioManager.STREAM_ALARM,
-                        newVolume,
-                        AudioManager.FLAG_SHOW_UI
-                );
-                break;
-            }
-            case RING: {
-                // Set the Ring volume
-                getAudioManager(context).setStreamVolume(
-                        AudioManager.STREAM_RING,
-                        newVolume,
-                        AudioManager.FLAG_SHOW_UI
-                );
-                break;
-            }
-            case MUSIC: {
-                // Set the Music volume
-                getAudioManager(context).setStreamVolume(
-                        AudioManager.STREAM_MUSIC,
-                        newVolume,
-                        AudioManager.FLAG_SHOW_UI
-                );
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("\nIllegal choice of Stream. Choose between \"ALARM\"," +
-                        " \"RING\" or \"MUSIC\" next time.");
-        }
+
+        // Sets the Volume.
+        getAudioManager(context).setStreamVolume(
+                getStream(context, stream),
+                newVolume,
+                AudioManager.FLAG_SHOW_UI
+        );
 
 
     }
@@ -141,53 +84,42 @@ public class AudioUtilityManager {
      * Sets up the Stream Volume to its maximum value (in percentage).
      *
      * @param context The current Context
-     * @param stream  The chosen Stream (should be"ALARM"/"RING"/"MUSIC", otherwise an exception is thrown)
-     * @throws IllegalArgumentException if parameter Stream is illegal
+     * @param stream  The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
      */
-    public static void setMaxVolume(Context context, String stream) throws IllegalArgumentException {
-        String streamUP = stream.toUpperCase();
-        if (!(streamUP.equals(ALARM) || streamUP.equals(RING) || streamUP.equals(MUSIC)))
-            throw new IllegalArgumentException("\nIllegal choice of Stream. Choose between \"ALARM\"," +
-                    " \"RING\" or \"MUSIC\" next time.");
-        else setVolume(context, stream, MAX_PERCENTAGE);
+    public static void setMaxVolume(Context context, Stream stream) {
+        setVolume(context, stream, MAX_PERCENTAGE);
     }
 
     /**
      * Sets up the Stream Volume to its minimum value (in percentage).
      *
      * @param context The current Context
-     * @param stream  The chosen Stream (should be"ALARM"/"RING"/"MUSIC", otherwise an exception is thrown)
-     * @throws IllegalArgumentException if parameter Stream is illegal
+     * @param stream  The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
      */
-    public static void setMinVolume(Context context, String stream) throws IllegalArgumentException {
-        String streamUP = stream.toUpperCase();
-        if (!(streamUP.equals(ALARM) || streamUP.equals(RING) || streamUP.equals(MUSIC)))
-            throw new IllegalArgumentException("\nIllegal choice of Stream. Choose between \"ALARM\"," +
-                    " \"RING\" or \"MUSIC\" next time.");
-        else setVolume(context, stream, MIN_PERCENTAGE);
+    public static void setMinVolume(Context context, Stream stream) {
+        setVolume(context, stream, MIN_PERCENTAGE);
     }
 
     /**
-     * Sets minimum Volume for Alarm, Ringtone and Music Streams.
-     *
      * @param context The current Context
+     * @param stream  The chosen Stream (should be ALARM, RING or MUSIC, otherwise an exception is thrown)
+     * @return The constant representing the stream.
+     * @throws IllegalArgumentException if stream is not valid (It shouldn't happen the enum).
      */
-    public static void setQuietMode(Context context) {
-        setMinVolume(context, ALARM);
-        setMinVolume(context, MUSIC);
-        setMinVolume(context, RING);
+    private static int getStream(Context context, Stream stream) throws IllegalArgumentException {
+
+        switch (stream) {
+            case ALARM:
+                return AudioManager.STREAM_ALARM;
+            case RING:
+                return AudioManager.STREAM_RING;
+            case MUSIC:
+                return AudioManager.STREAM_MUSIC;
+            default:
+                throw new IllegalArgumentException("\nIllegal Stream. It should be ALARM, RING or MUSIC .");
+        }
     }
 
-    /**
-     * Sets maximum Volume for Alarm, Ringtone and Media Streams.
-     *
-     * @param context The current Context.
-     */
-    public static void setLoudMode(Context context) {
-        setMaxVolume(context, ALARM);
-        setMaxVolume(context, MUSIC);
-        setMaxVolume(context, RING);
-    }
 
 }
 //TODO TESTING
