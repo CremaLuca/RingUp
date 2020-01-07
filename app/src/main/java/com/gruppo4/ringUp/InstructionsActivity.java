99fa41fd62f31e5231a41e06114c8f856448f770
francescobau <francescobau@users.noreply.github.com>
2020-01-07 19:22:28 +0100

    "Started" PermissionsActivity and InstructionActivity
    KNOWN ISSUES:
    - Francesco Bau'
    - Still undone. Retarded coder Francesco Bau' doesn't know what to do.

--- app/src/main/java/com/gruppo4/ringUp/InstructionsActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/InstructionsActivity.java
@@ -1,4 +1,11 @@
 package com.gruppo4.ringUp;
 
+/**
+ * [Insert class description here]
+ *
+ * @author Francesco Bau'
+ * @author [Insert new author's full name]
+ * @since 07/01/2020
+ */
 public class InstructionsActivity {
 }
--- app/src/main/java/com/gruppo4/ringUp/PermissionsActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/PermissionsActivity.java
@@ -1,4 +1,32 @@
 package com.gruppo4.ringUp;
 
-public class PermissionsActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * [Insert class description here]
+ *
+ * @author Francesco Bau'
+ * @author [Insert new author's full name]
+ * @since 07/01/2020
+ */
+public class PermissionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO: layout. setContentView è da modificare.
+        setContentView(R.layout.activity_main);
+
+        //TODO: Tutto. Non ci sto capendo.
+        // Bisogna forse creare un oggetto di tipo PasswordManager come in MainActivity???
+        if(!(checkSelfPermissions(TEXT_SERVICES_MANAGER_SERVICE) && isPassSaved()))
+        openPreActivities();
+    } else if(!(checkPermissions()))
+    openPermissionsActivity();
+else if(!(isPassSaved()))
+    openInstructionActivity();
+
+    }
 }
