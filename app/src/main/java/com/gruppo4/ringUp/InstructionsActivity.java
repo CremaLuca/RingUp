b671626fe8d04dc9c9b339f7d5ce289044bab361
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2020-01-08 01:06:27 +0100

    Initiated the pre activies

--- app/src/main/java/com/gruppo4/ringUp/InstructionsActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/InstructionsActivity.java
@@ -1,11 +1,23 @@
 package com.gruppo4.ringUp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 /**
- * [Insert class description here]
+ * Class used to inform the user on what ringUp is and what it is used for.
+ * It also requires the setting up of the device password
  *
  * @author Francesco Bau'
- * @author [Insert new author's full name]
+ * @author Alberto Ursino
  * @since 07/01/2020
  */
-public class InstructionsActivity {
+public class InstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+
 }
--- app/src/main/java/com/gruppo4/ringUp/PermissionsActivity.java
+++ app/src/main/java/com/gruppo4/ringUp/PermissionsActivity.java
@@ -1,32 +1,22 @@
 package com.gruppo4.ringUp;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 /**
- * [Insert class description here]
+ * Class used to inform the user of which permissions the app needed and requires them
  *
  * @author Francesco Bau'
- * @author [Insert new author's full name]
+ * @author Alberto Ursino
  * @since 07/01/2020
  */
 public class PermissionsActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO: layout. setContentView è da modificare.
-        setContentView(R.layout.activity_main);
-
-        //TODO: Tutto. Non ci sto capendo.
-        // Bisogna forse creare un oggetto di tipo PasswordManager come in MainActivity???
-        if(!(checkSelfPermissions(TEXT_SERVICES_MANAGER_SERVICE) && isPassSaved()))
-        openPreActivities();
-    } else if(!(checkPermissions()))
-    openPermissionsActivity();
-else if(!(isPassSaved()))
-    openInstructionActivity();
-
+        setContentView(R.layout.activity_permissions);
     }
+
 }
