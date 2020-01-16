8c52c46ad21c4f50639e27c07fde1e24fd302dec
Alberto Ursino <alberto.ursino@studentri.unipd.it>
2020-01-16 13:43:11 +0100

    Updated the master
    - Removed useless modules
    - Added implementations with JitPack (https://jitpack.io/)
    - Created the "app" module
    - Chaged the projec name to "ringUp"

--- app/src/androidTest/java/com/example/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/app/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
-package com.gruppo4.SMSApp;
+package com.example.app;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.gruppo_4.el_ing_sw", appContext.getPackageName());
+        assertEquals("com.example.app", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/app/ExampleUnitTest.java
+++ app/src/test/java/com/example/app/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.gruppo_4.preferences;
+package com.example.app;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
